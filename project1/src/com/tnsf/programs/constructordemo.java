3c10461f1eccd789423d90ecd692290672d5f47c
acer <acer@TMP249-G2-M.bbrouter>
2023-09-27 15:57:15 +0530

    first commit

---
+++ project1/src/com/tnsf/programs/constructordemo.java
@@ -0,0 +1,22 @@
+package com.tnsf.programs;
+
+import java.util.Scanner;
+
+public class constructordemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String name,city;
+		int id;
+		System.out.println("enter id");
+		id=sc.nextInt();
+		sc.nextLine();
+		System.out.println("enter city");
+		city=sc.nextLine();
+		System.out.println("enter name");
+		name=sc.nextLine();
+
+	}
+
+}

00c39e74c4f496e4922cd505c6f4e1b3dac99b27
acer <acer@TMP249-G2-M.bbrouter>
2023-09-27 16:11:24 +0530

    first commiit

---
+++ project1/src/com/tnsf/programs/hello.java
@@ -0,0 +1,11 @@
+package com.tnsf.programs;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+
+	}
+
+}

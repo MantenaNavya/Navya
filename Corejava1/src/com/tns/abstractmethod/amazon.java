f4ccbd5804f305a1d51b53cd19c4be0f8de6da24
acer <acer@TMP249-G2-M.bbrouter>
2023-10-07 10:13:17 +0530

    FIFTH COMMIT

--- Corejava1/src/com/tns/abstractmethod/amazon.java
+++ Corejava1/src/com/tns/abstractmethod/amazon.java
@@ -1,5 +1,15 @@
 package com.tns.abstractmethod;
+import java.util.Scanner;
 
 public class amazon {
+	public static void main(String[] args) {
+		int amt;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter amount");
+		amt=sc.nextInt();
+		Accounts a;
+		a=new Nonprime(amt);
+		a.delivarycharges(amt);
+	}
 
 }
